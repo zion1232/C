3916422ac9f52294559179be3dba18a21dd10659
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 22:14:19 +0200

    Create IF - ELSE - ELSEIF - SWITCH - 1.c

---
+++ IF - ELSE - ELSEIF - SWITCH/IF - ELSE - ELSEIF - SWITCH - 1.c
@@ -0,0 +1,54 @@
+#include <stdio.h> 
+#define _CRT_SECURE_NO_WARNINGS
+/*switch*/
+void main()
+{
+	int num_rooms, price, duplex;
+	printf("enter the num of rooms (3 - 4 - 5)");
+	scanf_s("%d", &num_rooms);
+	switch (num_rooms)
+	{
+	case 3:
+		price = 120;
+		break;
+	case 4:
+		price = 150;
+		break;
+	case 5:
+		printf("is duplex? ( 1 - yes/ 2 - no) :");
+		scanf_s("%d", &duplex);
+		if (duplex == 1) {
+			price = 200;
+		}
+		else {
+			price = 180;
+			break;
+		}
+
+	}
+	printf("please py me %d nis \n", price);
+}
+
+
+/*if and else*/
+void main()
+{
+	int num_rooms, price, duplex;
+	printf("enter the num of rooms (3 - 4 - 5)");
+	scanf_s("%d", &num_rooms);
+	if (num_rooms == 3)
+	{
+		price = 130;
+	}
+	else if (num_rooms == 4)
+	{
+		price = 150;
+	}
+	else 
+	{
+			printf("is duplex?");
+			scanf_s("%d", &duplex);
+			price = duplex ? 200 : 180;
+	}
+	printf("please py me %d nis \n", price);
+}
