587d2b289aa45312ad6ad8aeda9b8596d963c9e8
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 21:18:10 +0200

    Update packaging product.c

--- Basic Input Input/packaging product.c
+++ Basic Input Input/packaging product.c
@@ -1,7 +1,7 @@
 #include <stdio.h> Using libraries
 #define _CRT_SECURE_NO_WARNINGSCommand to use the SCAN command
 #define PRICE_FOR_PAPER 5;
-#define MAM 1.18;
+#define taxes 1.18;
 #define EXTRA_SIZE 1.12;
 void main()
 {
