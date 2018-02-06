749988bd6947a6d6ff6c5ab96aa22282ab9ac7f2
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 12:33:09 +0200

    Update Casting double to int.c

--- Basic Input Input/Casting double to int.c
+++ Basic Input Input/Casting double to int.c
@@ -1,11 +1,11 @@
 #include <stdio.h> /*Using libraries*/
-#define _CRT_SECURE_NO_WARNINGS/*Command to use the SCAN command*/*/
+#define _CRT_SECURE_NO_WARNINGS/*Command to use the SCAN command*/
 void main()
 {
 	double x;
 	printf("enter double number ");
 	scanf_s("%lf", &x);
-	printf("thr int of this nuber is %d\n ",(int)x);
+	printf("the int of this nuber is %d\n ",(int)x);
 
 
 }
