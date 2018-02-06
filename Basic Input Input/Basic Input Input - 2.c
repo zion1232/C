565d1577464feac1b0ea3ab48b5be62dd3d1fd81
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 13:34:50 +0200

    Create Basic Input Input - 2.c

---
+++ Basic Input Input/Basic Input Input - 2.c
@@ -0,0 +1,16 @@
+void main()
+{
+	double kilometers,total;
+	int bag;
+
+
+	printf("Welcome to the taxi \n");
+	printf("How far you have to travel in kilometers?\n");
+	scanf_s("%lf", &kilometers);
+	printf("How many bags do you have?\n");
+	scanf_s("%d", &bag);
+	total = bag * 2 + kilometers * 1.30 + 10.20 ;
+	printf("For this trip you have to pay\n %.2lf",total);
+
+
+}
