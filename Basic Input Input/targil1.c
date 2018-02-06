07b7482ca9e7ca85a75ed73ad1a0c6431cb40473
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 11:35:09 +0200

    Add files via upload

---
+++ Basic Input Input/targil1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> /*Using libraries*/
+#define _CRT_SECURE_NO_WARNINGS/*Command to use the SCAN command*/
+#define PRICE_FOR_TICKET 35;/*constant variable
+*/
+void main()
+{
+	int numberofti, pay;
+	printf("how mach tickets you buy");
+	scanf_s("%d", &numberofti);
+	pay = numberofti * PRICE_FOR_TICKET;
+	printf("You need to pay %d NIS",pay);
+
+
+}
