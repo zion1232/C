4ec18ebfcd8b727e4a7fc8cf6270de6fa3936046
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-08 00:48:04 +0200

    Update Count,Sum digits.c

--- Loops/Count,Sum digits.c
+++ Loops/Count,Sum digits.c
@@ -1,1 +1,29 @@
 
+/*Count digits in a number*/
+{
+	int num, c = 0;
+	printf("enter number");
+	scanf_s("%d", &num);
+	while (num != 0)
+	{
+
+		num = num / 10;
+		c++;
+	}
+	printf("%d", c);
+}
+
+/*sum the digits*/
+
+{
+	int num, sum = 0, temp;
+	printf("enter number");
+	scanf_s("%d", &num);
+	while (num>0)
+	{
+		temp = num % 10;
+		sum = sum + temp;
+		num = num / 10;
+	}
+	printf("%d", sum);
+}
