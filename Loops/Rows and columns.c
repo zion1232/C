40f14d2e8bf86c5a9f7c58fa7235c8a287209673
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-10 12:17:40 +0200

    Create Rows and columns.c

---
+++ Loops/Rows and columns.c
@@ -0,0 +1,13 @@
+rowa and columsn
+{
+	int i, j, r ,c;
+	printf("enter number of rowa and columsn");
+	scanf_s("%d%d", &r,&c);
+	for (i=0;i<r;i++)
+	{
+		for (j = 0; j<c; j++)
+			printf("*");
+		printf("\n");
+
+	}
+}
