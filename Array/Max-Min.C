7119fa44189b41986c100ba763e066227815de3b
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-16 13:00:58 +0200

    Update Max-Min.C

--- Array/Max-Min.C
+++ Array/Max-Min.C
@@ -1,32 +1,37 @@
-/* The high number in the array
+/* The high number in the array*/
 
 void main()
 {
-	int arr[SIZE], i,max,min;
+	int arr[SIZE], i,max;
 	printf("Please enter %d numbers : ", SIZE);
 	for (i = 0; i < SIZE; i++)
 		scanf_s("%d", &arr[i]);
-	max = arr[0];
+	max = 0;
 	for (i = 0; i < SIZE; i++)
 	{
-		if (arr[i] > max)
-			max = arr[i];
+		if (arr[i] > arr[max])
+			max = i;
 	}
-	printf("The max is %d : ", max);
+	printf("The max is at index %d  and its value is %d: ", max,arr[max]);
 
 	}
-	#####
-	The small number in the array
+	
+	
+	/*The small number in the array*/
 
+void main()
+{
+	int arr[SIZE], i,max;
 	printf("Please enter %d numbers : ", SIZE);
 	for (i = 0; i < SIZE; i++)
 		scanf_s("%d", &arr[i]);
-	min = arr[0];
+	max = 0;
 	for (i = 0; i < SIZE; i++)
 	{
-		if (arr[i] < min)
-			min = arr[i];
+		if (arr[i] < arr[max])
+			max = i;
 	}
-	printf("The max is %d : ", min);
-}
+	printf("The min is at index %d  and its value is %d: ", max,arr[max]);
+
+	}
 */
